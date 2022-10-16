9000c024a4deb4c69f82f587125f0791e1334b2f
Velimir Đurković <velimir.djurkovic@gmail.com>
2022-10-16 21:04:07 +0200

    Added password generator

--- src/main/java/com/example/demo/controller/PasswordController.java
+++ src/main/java/com/example/demo/controller/PasswordController.java
@@ -1,5 +1,6 @@
-package com.example.demo;
+package com.example.demo.controller;
 
+import com.example.demo.service.PasswordGenerator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/password")
 public class PasswordController {
+    private PasswordGenerator passwordGenerator;
+
+    public PasswordController(PasswordGenerator passwordGenerator) {
+        this.passwordGenerator = passwordGenerator;
+    }
 
     @GetMapping
     public ResponseEntity<String> Get() {
-        String generatedPassword = "8bg?vq$R>Y&aLQ9y";
+        String generatedPassword = passwordGenerator.generate();
         return ResponseEntity.ok(generatedPassword);
     }
 }
